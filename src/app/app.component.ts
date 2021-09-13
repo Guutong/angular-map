32ec0bd3fa98825ac1ef91d15cbce428fa937ed3
Guutong <pornmongkonp@gmail.com>
2021-09-13 15:03:47 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import DottedMap from 'dotted-map';
 
 @Component({
   selector: 'my-app',
@@ -8,21 +6,7 @@ import DottedMap from 'dotted-map';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  worldMap;
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.createMap();
-  }
-
-  createMap() {
-    const map = new DottedMap({ height: 90, grid: 'diagonal' });
-    const svgMap = map.getSVG({
-      radius: 0.22,
-      color: '#1FBC62',
-      shape: 'hexagon',
-      backgroundColor: '#F6FCF9'
-    });
-    this.worldMap = this.sanitizer.bypassSecurityTrustHtml(svgMap);
-  }
+  ngOnInit(): void {}
 }
